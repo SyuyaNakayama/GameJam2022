b37f61e3deb5eec81d190f7df998266f684a5d8a
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2022-09-05 23:23:35 +0900

    余分なファイル削除

--- Input.cpp
+++
@@ -1,9 +0,0 @@
-#include "Input.h"
-#include "DxLib.h"
-void Input::UpdateKeyState()
-{
-	for (size_t i = 0; i < 256; i++) { oldkey[i] = key[i]; };
-	GetHitKeyStateAll(key);
-}
-bool Input::isInput(const int KEY_NUM) { return key[KEY_NUM]; };
-bool Input::isTrigger(const int KEY_NUM) { return key[KEY_NUM] && !oldkey[KEY_NUM]; }
